ee2d91f9ab63a2cc4ad943b9c0375b93d00a71c9
Maiatog <gustavo.maiato04@gmail.com>
2025-09-23 10:43:57 -0300

    formulario pronto

--- script.js
+++ script.js
@@ -4,13 +4,25 @@ function mostrarDados() {
 }
 
 function processarDados(event) {
+  const nome = document.getElementById("nome").value;
   const senha = document.getElementById("senha").value;
   const confirmSenha = document.getElementById("confirmSenha").value;
 
   /*Verifica se as senhas são iguais*/
   if (senha === confirmSenha) {
+    localStorage.setItem("nome", nome);
     alert("Cadastro realizado com sucesso");
   } else {
     alert("As senhas não correspondem!");
   }
 }
+document.querySelectorAll('input[name="sexo"]').forEach(function(radio) {
+  radio.addEventListener('change', function() {
+    const campoOutro = document.getElementById('campoOutroGenero');
+    if (this.value === 'outro') {
+      campoOutro.style.display = 'block';
+    } else {
+      campoOutro.style.display = 'none';
+    }
+  });
+});
