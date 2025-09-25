0d13019b819491c9a739fe624cee8ce853273b21
Maiatog <gustavo.maiato04@gmail.com>
2025-09-25 11:18:19 -0300

    first commit

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-function mostrarDados() {
-  const nome = document.getElementById("nome").value;
-  document.getElementById("meuFormulario").innerHTML = nome;
+function mudaimg(){
+    let imagem = Math.floor(Math.random() * 100);
 }
-
-function processarDados(event) {
-  const nome = document.getElementById("nome").value;
-  const senha = document.getElementById("senha").value;
-  const confirmSenha = document.getElementById("confirmSenha").value;
-
-  /*Verifica se as senhas são iguais*/
-  if (senha === confirmSenha) {
-    localStorage.setItem("nome", nome);
-    alert("Cadastro realizado com sucesso");
-  } else {
-    alert("As senhas não correspondem!");
-  }
-}
-document.querySelectorAll('input[name="sexo"]').forEach(function(radio) {
-  radio.addEventListener('change', function() {
-    const campoOutro = document.getElementById('campoOutroGenero');
-    if (this.value === 'outro') {
-      campoOutro.style.display = 'block';
-    } else {
-      campoOutro.style.display = 'none';
-    }
-  });
-});
